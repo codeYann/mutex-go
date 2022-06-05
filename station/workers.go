3e881e9182fb98c403076121b8656acb7de66e6e
codeYann <ccyan05@alu.ufc.br>
2022-06-05 15:18:48 -0300

    Adding comments on workers file

--- station/workers.go
+++ station/workers.go
@@ -1,11 +1,13 @@
 package station
 
+// Estrutura para os funcionários
 type Worker struct {
 	name string
 	id   int
 	role string
 }
 
+// Criando um funcionário
 func CreateWorker(nameWorker, roleWorker string, idWorker int) *Worker {
 	return &Worker{
 		name: nameWorker,
